f1f0c90392204bf662a23d7732a2ee8d296d8301
maxyaka-1 <maxyaka.dudkin@mail.ru>
2025-01-17 22:13:07 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+username = input(" Имя пользователя ")
+title = input(" Заголовок заметки ")
+content = input(" Описание заметки ")
+status = input(" Статус заметки ")
+created_date = input(" ДД-ММ-ГГГГ ")
+print(created_date[:-5])
+issue_date = input(" ДД-ММ-ГГГГ ")
+print(issue_date[:-5])

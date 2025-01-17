c0a76193102ac433f9bf863709eab95449b0ce49
maxyaka-1 <maxyaka.dudkin@mail.ru>
2025-01-17 21:28:33 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+temp_issue_date = "17-01-2025"
+print(temp_issue_date[:5])
+temp_created_date = "17-02-2025"
+print(temp_created_date[:5])
+temp_issue_date = "17-января-2025"
+print(temp_issue_date[:-5])
+temp_created_date = "17-февраля-2025"
+print(temp_created_date[:-5])
